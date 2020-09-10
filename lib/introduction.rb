8bd744077f52b51ed0bf388b8f8417393e5f66e2
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-09-10 17:07:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
 def introduction(name)
-  puts "Hi, my name is  #{name}."
+  puts "Hi, my name is #{name}."
 end
-def greeting_two(name, language)
-  puts "Hello #{name}, i see you speak #{language}"
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
